c434c0400eab8dfa43f42d2c78b452eef38e59df
justpiyoosh <justpiyoosh@gmail.com>
2020-07-17 12:54:54 +0530

    Configured for Deploy

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -13,5 +13,5 @@ router.register('feed',views.UserProfileFeedViewSet)
 urlpatterns = [
     path('hello-view',views.HelloApiView.as_view()),
     path('login/',views.UserLoginApiView.as_view()),
-    path('',include(router.urls))
+    path('',include(router.urls)),
 ]
